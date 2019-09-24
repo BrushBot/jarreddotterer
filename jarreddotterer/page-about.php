05038cf5f00fdd31160c19d349d665b1e2032bcb
Jarred Dotterer <jarred@brushbot.com>
2019-09-24 07:48:10 -0600

    Update About Us

--- jarreddotterer/page-about.php
+++ jarreddotterer/page-about.php
@@ -6,29 +6,36 @@ Template Name: About
 
 <?php get_header(); ?>
 
-<div class="wrapper main">
+<main class="wrapper main">
 
 	<h1 class="image-title">
 
-		<img src="<?php bloginfo( 'template_url' ); ?>/images/about.png" alt="Journal">
+		<img 
+			src="<?php bloginfo( 'template_url' ); ?>/images/about.png"
+			alt="Jarred Head Illustration"
+		/>
 		
 	</h1>
-	
+
 	<div class="content">
 	
-		<p>Jarred is a web designer and developer with four years professional experience.</p>
+		<p>Jarred is a UI Engineer with eight years professional experience. He cares about the entire development cycle. Besides engineering he also contributes to user interface design, user experience, API architecture, documentation, and team chemistry. Jarred is a creative problem solver who is willing to learn new skills to drive his team towards success.</p>
 		
-		<p>He is passionate about creating positive experiences for his clients and their users. When designing he believes content is king, and user experience should always be of utmost importance. He codes in HTML, CSS, Javascript, C# Razor, and PHP. He is familiar with the JS frameworks: JQuery, Knockout, Angular, and TypeAhead. He is skilled at turning WordPress into a friendly, easy to use CMS. He also excels at working in ASP.Net MVC views, and has 2 years experience working on an MVC application at PRNewswire.</p>
+		<p>When creating software Jarred focuses on the people who create and consume it. He thinks user experience is of utmost importance. Jarred is emotionally intelligent. He codes in HTML, CSS/LESS/SASS, JavaScript, TypeScript, C#, SQL, and PHP. He is familiar with the JS frameworks: JQuery, Knockout, Angular.js, Angular 2+, Underscore, and TypeAhead. Jarred believes in following the Agile process closely. He is flexible and handles reshuffling priorities like a champion.</p>
 		
-		<p>When Jarred is not working on on websites he is often found spending time with his beautiful wife Kristen and daughter Annalyse. Together, they love hiking and spending time in the Rocky Mountains and taking photographs of dragonflies. He is also a gamer, snowboarder, saxophonist, guitarist, and singer.</p>
+		<p>When Jarred is not at work, he can be found spending time with his beautiful wife Kristen and two daughters. Together, they love to hike, camp, dance, play board games, and be silly. Jarred is also a snowboarder, saxophonist, guitarist, singer, D&D player, miniature painter, and sports enthusiast.</p>
 		
 		<p>If you haven't already, check out some of his <a href="/work">work</a>.</p>
 		
 	</div>
 	
-	<img class="about-photo" src="<?php bloginfo( 'template_url' ); ?>/images/family.jpeg" alt="Jarred, Kristen, and Annalyse Dotterer">
+	<img
+		class="about-photo"
+		src="<?php bloginfo( 'template_url' ); ?>/images/dotterer-family.jpg"
+		alt="Jarred, Kristen, and kids"
+	/>
 
 
-</div>
+</main>
 	
 <?php get_footer(); ?>
